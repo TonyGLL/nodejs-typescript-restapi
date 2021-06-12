35562af6165fe52e8c0e80287580c0580629b591
TonyGLL <tonygllambia@gmail.com>
2021-06-12 17:51:54 -0500

    dist ignore

--- dist/main/main.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mainRouter = void 0;
-const express_1 = require("express");
-const users_routes_1 = require("../users/users.routes");
-exports.mainRouter = express_1.Router();
-exports.mainRouter
-    .use(users_routes_1.usersRouter);
